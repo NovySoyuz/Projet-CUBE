46f069e9a5564c92a82cab637ef7d6039191a2b7
Agathe <137417989+Agathe-15@users.noreply.github.com>
2023-12-18 13:42:42 +0100

    main.py
    Modification des commentaires

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# Import du framework Flask 
-# Bibliotheque request = envoyer des requetes http
-# Bibliotheque jsonify = convertir les données en json
-# Bibliotheque render_template = permet de récuprer des fichiers dans le dossier templates
+# Bibliotheque Request, Jsonify, render_template, Flask_cors, Mysql.connector
+
 from flask import Flask, request, jsonify, blueprints
-# Bibliotheque flask_cors = permet, à travers des en-têtes HTTP, d'autoriser un utilisateur l'accès à des resources situées sur une autre origine que le site courant
 from flask_cors import CORS
-# Bibliotheque mysql.connector = connexion à une bdd
 from database import db, cursor, create_database
-# On importe data du fichier auth pour pouvoir aller sur la route 
+# Importation des fichiers
 from auth import data
-# dotenv permet d'utiliser et de charger le contenu d'un fichier .env
 from dotenv import load_dotenv
-# Chargement des variables dans le .env
+# Variables dans le .env
 create_database()
 load_dotenv()
 # Lancement d'un script python
@@ -22,39 +17,28 @@ app.register_blueprint(data, url_prefix='/data')
 CORS(app)
 # Route pour recevoir et stocker les données de la sonde
 # Creation de la route /releves accessible via POST
-# Argument methods (on aurait pu remplacer par endpoint ou autre) pour pouvoir utiliser la route
 @app.route('/releves', methods=['GET','POST','PUT','DELETE'])
-# definition d'une fonction recuperant des donnees reçu par la sonde
 
+# Donner recupéré par la sonde methode post
 def receive_data():
     if request.method == 'POST':
-    # Si la méthode est POST, récupérez les données de la requête en JSON
         data = request.json
         temperature = data.get('temperature')
         humidity = data.get('humidity')
-        # Stockage des données dans la base de données
-        # execution de la requete sql pour inserer les temperatures
-        # %s = sécurisation des requetes sql
         cursor.execute("INSERT INTO table_des_releves (temperature, humidity, date) VALUES (%s, %s, NOW())", (temperature, humidity))
-        # Validation et sauvagarde des modifications de la BDD
         db.commit()
-        
-        #reponse JSON de la bonne execution
         return jsonify({'message': 'Données reçues et stockées'})
-    
+    #Methode Get
     if request.method == 'GET':
-        # Si la méthode est GET, récupérez toutes les données de la table_des_releves
         
         cursor.execute("SELECT * FROM table_des_releves")
-        # Récupère toutes les lignes du résultat
         data = cursor.fetchall()
 
         # Convertissez les données en format JSON
         response = jsonify(data)
         return response
-    
+    #methode Put
     if request.method == 'PUT':
-        # Si la méthode est PUT, récupérez toutes les données de la table_des_releves
         releve_id = request.args.get('releve_id')
         temperature = request.json.get('temperature')
         humidity = request.json.get('humidity')
