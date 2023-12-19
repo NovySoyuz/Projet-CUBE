b88dfb8650f3b34577b6c721b38471e7b88a9637
NovySoyuz <150264637+NovySoyuz@users.noreply.github.com>
2023-12-19 19:06:25 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,19 +3,50 @@ from database import cursor
 from dotenv import load_dotenv
 load_dotenv()
 data = Blueprint('data',__name__)
+
 # Route vers le site web
 # Recuperation des donner 
 # Les route permettent de venir récup les données via le navigateur 
 @data.route('/site')
 def site_dashboard():
+    """
+    Route pour afficher la page du site web.
+    ---
+    responses:
+      200:
+        description: Page du site affichée avec succès.
+    """
     return render_template("Page_Donee_Meteo.html")
 
 @data.route('/Page_du_Graphique.html')
 def graphique_dashboard():
+    """
+    Route pour afficher la page du graphique.
+    ---
+    responses:
+      200:
+        description: Page du graphique affichée avec succès.
+    """
     return render_template("Page_du_Graphique.html")
 
 @data.route('/donnees_graphiques', methods=['GET'])
 def get_graph_data():
+    """
+    Récupère les données graphiques.
+    
+Cette route renvoie les données nécessaires pour afficher un graphique.
+    
+---
+responses:
+  200:
+    description: |
+      Succès. Renvoie les données graphiques au format JSON.
+      Voici un exemple :
+      labels: ['2022-01-01', '2022-01-02', '2022-01-03']
+      temperature: [25.0, 26.5, 24.8]
+      humidity: [60, 62, 58]
+        
+    """   
     cursor.execute("SELECT date, temperature, humidity FROM table_des_releves")
     data = cursor.fetchall()
     
